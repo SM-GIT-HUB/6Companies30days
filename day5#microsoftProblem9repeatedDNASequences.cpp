2f8c3a9762ceb51ca0cfb6eecd056031ce3f68f2
Soumik Majumder <158304400+SM-GIT-HUB@users.noreply.github.com>
2025-01-05 11:37:44 +0530

    Create day5#microsoftProblem9repeatedDNASequences.cpp

---
+++ day5#microsoftProblem9repeatedDNASequences.cpp
@@ -0,0 +1,28 @@
+vector<string> findRepeatedDnaSequences(string s) {
+        int n = s.size();
+
+        if (n < 10) {
+            return {};
+        }
+
+        vector<string> ans;
+
+        unordered_map<string, int> mapp;
+        string output = s.substr(0, 10);
+
+        mapp[output] = 1;
+
+        for (int i = 1; i + 9 < n; i++)
+        {
+            output.erase(0, 1);
+            output += s[i + 9];
+
+            if (mapp[output] == 1) {
+                ans.push_back(output);
+            }
+            
+            mapp[output]++;
+        }
+
+        return ans;
+    }
